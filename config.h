ad063824630772ce9db0427511b93349b0eb15cb
gabriwar <gabrielduarteg02@gmail.com>
2021-11-17 18:38:53 +0000

    cant remember now but is a simple commit

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"sb-moonphase",	18000,	17}, 
+	/*{"",	"sb-moonphase",	18000,	17},*/ 
 	{"", "sb-kbselect", 0, 30},
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
